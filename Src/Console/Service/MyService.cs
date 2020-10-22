ae0662f4ef74dd6bb94945b4c59353fc2f3c226d
keuschni <Marko.Keuschnig-EE@infineon.com>
2020-10-22 14:49:16 +0200

    initial commit

---
+++ Src/Console/Service/MyService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Flurl.Http;
+
+namespace ConsoleApplication.Service
+{
+    public class MyService
+    {
+        public async Task<bool> DoAsync(int retry = 1)
+        {
+            HttpResponseMessage response;
+            try
+            {
+                response = await "http://www.google.com".GetAsync();
+            }
+            catch (FlurlHttpException e)
+            {
+                if (retry <= 3)
+                {
+                    Thread.Sleep(new TimeSpan(0, 0, 1 * retry));
+                    return await DoAsync(++retry);
+                }
+                Console.WriteLine(e);
+                throw;
+            }
+
+            return response.IsSuccessStatusCode;
+        }
+    }
+}
---
+++ Test/Console.Tests/Tests/MyServiceTests.cs
@@ -0,0 +1,36 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using ConsoleApplication.Service;
+using Flurl.Http.Testing;
+using Shouldly;
+using Xunit;
+
+namespace ConsoleApplication.Tests.Tests
+{
+    public class MyServiceTests
+    {
+        [Fact]
+        public async Task DoAsyncTest()
+        {
+            using var httpTest = new HttpTest();
+            httpTest.RespondWith("Internal Server Error", 500);
+            var service = new MyService();
+            var isSuccessStatusCode = await service.DoAsync();
+            isSuccessStatusCode.ShouldBeTrue();
+        }
+
+        [Fact]
+        public async Task DoAsyncTest02()
+        {
+            using var httpTest = new HttpTest();
+            httpTest.RespondWith("OK", 200);
+            var service = new MyService();
+            
+            var isSuccessStatusCode = await service.DoAsync();
+
+            isSuccessStatusCode.ShouldBeTrue();
+            httpTest.ShouldHaveCalled("http://www.google.com")
+                .WithVerb(HttpMethod.Get);
+        }
+    }
+}
